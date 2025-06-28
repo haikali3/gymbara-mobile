8b8319acbc87c5aff6eea4253f40a3dfe9ad161f
haikali3 <manfdvcl9@gmail.com>
2025-06-29 07:33:24 +0800

    refactor: clean up imports and improve formatting in TabTwoScreen and Text components

--- app/(tabs)/tab-two.tsx
+++ app/(tabs)/tab-two.tsx
@@ -1,25 +1,5 @@
-import { Info } from "lucide-react-native";
-import { View, Text, Animated, Button } from "react-native";
-import {
-  LayoutAnimationConfig,
-  FadeInUp,
-  FadeOutDown,
-} from "react-native-reanimated";
-import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Progress } from "~/components/ui/progress";
-import {
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-} from "~/components/ui/tooltip";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
 
 export default function TabTwoScreen() {
   return (
@@ -28,6 +8,7 @@ export default function TabTwoScreen() {
       <Text className="text-muted-foreground text-xl font-bold">
         Tab Two Screen
       </Text>
+      <Text className="">Normal</Text>
     </View>
   );
 }
