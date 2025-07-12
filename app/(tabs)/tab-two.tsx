ae5d9d9f393fa29833514e41c9beeb3b5f1e1ff3
haikali3 <manfdvcl9@gmail.com>
2025-07-12 23:06:39 +0800

    lint

--- app/(tabs)/tab-two.tsx
+++ app/(tabs)/tab-two.tsx
@@ -2,13 +2,13 @@ import { View } from "react-native";
 import { Text } from "~/components/ui/text";
 
 export default function TabTwoScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-secondary/30">
-      <Text className="text-foreground text-xl font-bold">Tab Two Screen</Text>
-      <Text className="text-muted-foreground text-xl font-bold">
-        Tab Two Screen
-      </Text>
-      <Text className="">Normal</Text>
-    </View>
-  );
+	return (
+		<View className="flex-1 items-center justify-center bg-secondary/30">
+			<Text className="text-foreground text-xl font-bold">Tab Two Screen</Text>
+			<Text className="text-muted-foreground text-xl font-bold">
+				Tab Two Screen
+			</Text>
+			<Text className="">Normal</Text>
+		</View>
+	);
 }
--- app/workout/_layout.tsx
+++ app/workout/_layout.tsx
@@ -2,24 +2,24 @@ import { Stack } from "expo-router";
 import { ThemeToggle } from "~/components/ThemeToggle";
 
 export default function WorkoutLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerRight: () => <ThemeToggle />,
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Workout",
-        }}
-      />
-      <Stack.Screen
-        name="plans"
-        options={{
-          title: "Workout Plans",
-        }}
-      />
-    </Stack>
-  );
+	return (
+		<Stack
+			screenOptions={{
+				headerRight: () => <ThemeToggle />,
+			}}
+		>
+			<Stack.Screen
+				name="index"
+				options={{
+					title: "Workout",
+				}}
+			/>
+			<Stack.Screen
+				name="plans"
+				options={{
+					title: "Workout Plans",
+				}}
+			/>
+		</Stack>
+	);
 }
